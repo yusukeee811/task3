fed2db119a5d81db83d1f03e750def75b498b3e9
yusukeee811 <132319174+yusukeee811@users.noreply.github.com>
2023-05-17 12:42:09 +0000

    [Update]Bootstarp導入

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,31 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
+  end
+  
+  def index
+    @books = Book.all
+  end
+  
+  def show
+    @book = Book.find(params[:id])
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to book_
+  end
+    
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body, :image)
+  end
 end
